57e1fb7e085318e963884ee55a536e6877338101
nickmutogmailcom <58951083+nickmutogmailcom@users.noreply.github.com>
2026-04-01 00:18:03 +0200

    Aggiorna main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,17 +2,37 @@ input.onButtonPressed(Button.A, function () {
     if (attesa > 100) {
         attesa += -100
     } else {
-        strip.showColor(neopixel.rgb(100, 0, 0))
-        basic.pause(2000)
+        c_blu = 0
+        c_green = 100
+        c_red = 0
+        strip.showColor(neopixel.rgb(c_red, c_green, c_blu))
+        basic.pause(500)
         attesa = 1000
     }
+    CambioColore = 0
 })
 input.onButtonPressed(Button.B, function () {
     attesa = 1000
-    c_blu = 100
-    c_green = 0
-    c_red = 0
+    if (CambioColore == 0) {
+        c_blu = 100
+        c_green = 0
+        c_red = 0
+    } else if (CambioColore == 1) {
+        c_blu = 0
+        c_green = 0
+        c_red = 100
+    } else if (CambioColore == 2) {
+        c_blu = 0
+        c_green = 100
+        c_red = 100
+    } else {
+        c_blu = 100
+        c_green = 100
+        c_red = 100
+    }
+    CambioColore += 1
 })
+let CambioColore = 0
 let c_red = 0
 let c_green = 0
 let c_blu = 0
